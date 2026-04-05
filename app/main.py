e68b9a157a09b203f8a1d03fe4e5f99cdd4b63ac
Im-Jongseok <im.jongseoklee@gmail.com>
2026-04-05 12:10:09 +0900

    Add: 입력 예외처리 함수

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ def main():
     print_menu()
     while True:
         option = choose_option()
-        if option == 5:
+        if option == 5 :
             print("게임을 종료합니다.")
+            break
+        elif option == EOFError:
             break
 
         handle_option(option)
+
     return 0
 
 def print_menu():
@@ -22,23 +25,47 @@ def print_menu():
     print("==============================\n")
 
 def choose_option():
-    option = int(input("선택: "))
+    option = get_valid_input(1, 5, "옵션 선택: ")
     return option
 
+def get_valid_input(min_val, max_val, prompt):
+     while True:
+        try:
+            raw = input(prompt)
+
+            # 빈 입력 처리
+            if raw.strip() == "":
+                print("EMPTY INPUT ERROR: 값을 입력해주세요.")
+                continue
+
+            # 앞뒤 공백 제거 후 숫자 변환
+            value = int(raw.strip())
+
+            # 허용 범위 검사
+            if not (min_val <= value <= max_val):
+                print(f"RANGE ERROR: {min_val}~{max_val} 사이의 숫자를 입력해주세요.")
+                continue
+
+            return value
+
+        except ValueError:
+            print("TYPE ERROR: 숫자만 입력해주세요.")
+
+        except (KeyboardInterrupt, EOFError):
+            print("\n EOF ERROR: 프로그램을 안전하게 종료합니다.")
+            return EOFError
+    
 def handle_option(option):
     if(option == 1) :
-        print("퀴즈 풀기\n")
+        print("퀴즈 풀기")
     elif(option == 2):
-        print("퀴즈 추가\n")
+        print("퀴즈 추가")
     elif(option == 3):
-        print("퀴즈 목록\n")
+        print("퀴즈 목록")
     elif(option == 4):
-        print("점수 확인\n")
+        print("점수 확인")
     elif(option == 5):
-        print("종료\n")
-    else:
-        print("잘못된 입력입니다. 다시 입력해주세요.\n")
-        return False
+        print("종료")
     return True
 
 if __name__ == "__main__":
